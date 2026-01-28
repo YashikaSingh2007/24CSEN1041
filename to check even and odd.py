3e6420b73b162119fbd5c294507a8bfb384758c7
YashikaSingh2007 <ksingh1@student.gitam.edu>
2026-01-28 11:16:21 +0530

    Update to check even and odd.py

--- to check even and odd.py
+++ to check even and odd.py
@@ -10,3 +10,18 @@ elif x < 0:
     print(x,"is negative")
 else:
     print(x,"is zero")
+
+
+# OUTPUT:
+1. Enter an integer (positive or negative): -8
+-8 is negative
+
+2.Enter an integer (positive or negative): 9
+9 is positive
+9 is odd
+
+3.Enter an integer (positive or negative): 4
+4 is positive
+4 is even
+
+
