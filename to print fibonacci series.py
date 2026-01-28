d0fe46d50a51f4a2ffac25e697751d00907a1daa
YashikaSingh2007 <ksingh1@student.gitam.edu>
2026-01-28 11:01:01 +0530

    Add files via upload

---
+++ to print fibonacci series.py
@@ -0,0 +1,18 @@
+terms = int(input("Enter the number of terms (greater than 2): "))
+f1 = 0
+f2 = 1
+f3 = f1 + f2
+
+print("Fibonacci Series:")
+print(f1)
+print(f2)
+
+i = 3
+while True:
+    print(f3)
+    f1 = f2
+    f2 = f3
+    f3 = f1 + f2
+    i += 1
+    if i > terms:
+        break
