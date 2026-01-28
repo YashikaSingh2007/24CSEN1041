bced21d58148179cbe07c5a551b2699ab0f60289
YashikaSingh2007 <ksingh1@student.gitam.edu>
2026-01-28 11:33:22 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -9,3 +9,19 @@ for i in range(length):
     password += random.choice(characters)
 
 print("The generated password is:", password)
+
+
+# OUTPUT:
+Enter password length: 12
+The generated password is: bZw/`]%F~hv/
+
+Enter password length: 5
+The generated password is: r(%c_
+
+Enter password length: 26
+The generated password is: {YO#d)]KZ,(=MW}+x_s%4+!w(s
+
+
+
+
+
