9c3666bb2976e8271ca3ce4f9a0947c8e00852f2
YashikaSingh2007 <ksingh1@student.gitam.edu>
2026-01-29 14:47:26 +0530

    Update Number guessing system code.py

--- Number guessing system code.py
+++ Number guessing system code.py
@@ -13,3 +13,19 @@ while True:
         print("Wow! You guessed it correctly")
         print("Total attempts:", attempts)
         break
+
+
+# OUTPUT:
+Guess a number between 1 and 100: 56
+Too low! Try again
+Guess a number between 1 and 100: 78
+Too low! Try again
+Guess a number between 1 and 100: 92
+Too high! Try again
+Guess a number between 1 and 100: 90
+Too low! Try again
+Guess a number between 1 and 100: 91
+Wow! You guessed it correctly
+Total attempts: 5
+
+
