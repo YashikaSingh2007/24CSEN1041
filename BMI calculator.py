aad895bc915d169060e8ef1013cdc31ea0f7b226
YashikaSingh2007 <ksingh1@student.gitam.edu>
2026-03-05 14:32:32 +0530

    Update BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -6,3 +6,15 @@ height = float(input("Enter the height:"))
 
 bmi = calculate_bmi(weight,height)
 print(f"Your BMI is: {bmi: .2f}")
+
+
+
+OUTPUT:
+Enter the weight:70.0
+Enter the height:1.75
+Your BMI is:  22.86
+
+Enter the weight:50.5
+Enter the height:1.65
+Your BMI is:  18.55
+
