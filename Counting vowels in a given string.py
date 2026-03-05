ce08492908c40bc5e1d5fba4b6c50984344653a9
YashikaSingh2007 <ksingh1@student.gitam.edu>
2026-03-05 14:52:39 +0530

    Update Counting vowels in a given string.py

--- Counting vowels in a given string.py
+++ Counting vowels in a given string.py
@@ -11,3 +11,15 @@ input_string= input("Enter the string:")
 result= count_vowels(input_string)
 
 print("Number of vowels in the string:", result)
+
+
+OUTPUT:
+Enter the string:Hello, World!
+Number of vowels in the string: 3
+
+Enter the string:Python is a great programming language
+Number of vowels in the string: 12
+
+Enter the string:APPle
+Number of vowels in the string: 2
+
