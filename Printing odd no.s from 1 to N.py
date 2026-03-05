24203efb8a08371dcbb8c45d9031a97c4ca55a5f
YashikaSingh2007 <ksingh1@student.gitam.edu>
2026-03-05 15:15:22 +0530

    Update Printing odd no.s from 1 to N.py

--- Printing odd no.s from 1 to N.py
+++ Printing odd no.s from 1 to N.py
@@ -9,3 +9,16 @@ for i in range(1, N+1):
             first = False
         else:
             print(",",i,sep="",end="")
+
+
+
+OUTPUT:
+Enter the number:11
+1,3,5,7,9,11
+
+Enter the number:8
+1,3,5,7
+
+Enter the number:1
+1
+
